81591b365e24fd142973182203101c4b10e8f8ef
Scott Cooper <scttcper@gmail.com>
2016-11-22 10:37:20 -0800

    error

--- examples/example.js
+++ examples/example.js
@@ -1,47 +1,18 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const router = require('koa-router')();
 const Raven = require('raven');
 
-const koaRaven = require('../index');
-
-const app = module.exports = new Koa();
-
-app.use(bodyParser());
-
-const client = app.context.raven = Raven
-  .config('https://public:private@app.getsentry.com/269')
+const app = new Koa();
+const client = Raven
+  .config(false)
   .install({ unhandledRejection: true });
 
-koaRaven(app, client);
-
-router.get('/', (ctx, next) => {
-  ctx.body = 'ok';
-  return next();
+app.on('error', (err, ctx) => {
+  const parsedReq = client.parsers.parseRequest(ctx.request);
+  client.captureException(err, parsedReq);
 });
 
-router.get('/throw', () => {
+app.use(() => {
   throw new Error('Terrible Error');
 });
 
-router.post('/throwPost', () => {
-  throw new Error('Terrible Error');
-});
-
-router.get('/throwUser', (ctx) => {
-  ctx.body = 'ok';
-  Raven.setContext({
-    user: {
-      email: 'matt@example.com',
-      id: '123',
-    },
-  });
-  throw new Error('Terrible Error');
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-if (!module.parent) {
-  app.listen(3000);
-}
+app.listen(3000);
