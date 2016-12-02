e9d6b59823c80c2b6a02923e811e38adcefe9f17
Scott Cooper <scttcper@gmail.com>
2016-12-02 10:51:44 -0800

    ignore non server errors

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = function sentry(app, client) {
   app.on('error', (err, ctx) => {
+    // ignore non server errors
+    if (ctx.status < 500) {
+      return;
+    }
     const parsedReq = client.parsers.parseRequest(ctx.request);
     client.captureException(err, parsedReq);
   });
